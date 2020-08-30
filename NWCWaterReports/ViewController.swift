09c69e6b3258742bc0c0884325396b9909402e5c
Mohamed Sadek <msadek.C@nwc.com.sa>
2020-08-30 17:56:26 +0200

    finished design of submitting report and its viewController

--- NWCWaterReports/ViewController.swift
+++ NWCWaterReports/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var mobileTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    
+    //MARK: lifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      customizeNavigationBar()
     }
 
+    //MARK: Methods
+    func customizeNavigationBar() {
+      if let navController = navigationController {
+            title = "بلاغات المياه"
+            navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "back sign"), style: .done, target: self, action: #selector(backBtnPressed))
+            
+           navController.navigationBar.barTintColor =  #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+           navController.navigationBar.tintColor = #colorLiteral(red: 1, green: 0.99997437, blue: 0.9999912977, alpha: 1)
+        }
+      }
 
+
+    @objc func backBtnPressed(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: Actions
+    @IBAction func submitBtnAction(_ sender: UIButton) {
+    }
 }
 
