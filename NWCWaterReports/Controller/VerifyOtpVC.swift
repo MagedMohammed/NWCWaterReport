348548283c6aca3ee84fa90cc5d9bcaa745a9de6
Mohamed Sadek <msadek.C@nwc.com.sa>
2020-09-03 14:55:02 +0200

    add history button in sideMenu and handle it's action and localize VerifyOtp viewController and edit in localization in personalDetailsViewCOntroller

--- NWCWaterReports/Controller/VerifyOtpVC.swift
+++ NWCWaterReports/Controller/VerifyOtpVC.swift
@@ -11,8 +11,17 @@ import UIKit
 class VerifyOtpVC: UIViewController {
 
     //MARK: IBOutlets
+    @IBOutlet weak var headerLabel: UILabel!{
+        didSet{
+            headerLabel.text = "enter_the_sent_code".localized()
+        }
+    }
     @IBOutlet weak var verifyCodeTF: UITextField!
-    @IBOutlet weak var VerfiyBtn: UIButton!
+    @IBOutlet weak var VerfiyBtn: UIButton!{
+        didSet {
+            VerfiyBtn.setTitle("verify".localized(), for: .normal)
+        }
+    }
     
     //MARK: lifeCycle Methods
     override func viewDidLoad() {
@@ -22,9 +31,6 @@ class VerifyOtpVC: UIViewController {
      //MARK: Methods
     func customizeNavigationBar() {
       if let navController = navigationController {
-        title = "Verification".localized()
-            navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "back sign"), style: .done, target: self, action: #selector(backBtnPressed))
-            
            navController.navigationBar.barTintColor =  #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
            navController.navigationBar.tintColor = #colorLiteral(red: 1, green: 0.99997437, blue: 0.9999912977, alpha: 1)
         }
